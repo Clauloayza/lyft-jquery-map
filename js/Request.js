56733ddc8d37fa51aeaf51da53b0fcfe2bbff62d
claudia <clou7loymed@gmail.com>
2017-03-03 01:18:14 -0500

    request

--- js/Request.js
+++ js/Request.js
@@ -3,10 +3,12 @@ function initMap() {
  // Create a map object and specify the DOM element for display.   
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -16.457389199999998, lng: -71.5315308},
-    zoom:14
+    zoom:14,
+    disableDefaultUI: true
   });
     
     var myLatLng = {lat: -16.457389199999998, lng: -71.5315308};
+    
 
   var marker = new google.maps.Marker({
     position: myLatLng,
@@ -15,7 +17,8 @@ function initMap() {
 	icon: "image/carpink (2).png",
 	
   });
-	
+ 
+    
 	var geocoder = new google.maps.Geocoder();
 	
 	document.getElementById('submit').addEventListener('click', function() {
@@ -23,6 +26,8 @@ function initMap() {
   });
 }
 
+
+//buscador-------------
 function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
--- js/Set-pickut.js
+++ js/Set-pickut.js
@@ -4,10 +4,12 @@ function initMap() {
  // Create a map object and specify the DOM element for display.   
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -16.457389199999998, lng: -71.5315308},
-    zoom:14
+    zoom:14,
+    disableDefaultUI: true
   });
     
     var myLatLng = {lat: -16.457389199999998, lng: -71.5315308};
+    
 
   var marker = new google.maps.Marker({
     position: myLatLng,
@@ -15,5 +17,12 @@ function initMap() {
     title: 'Hello World!',
 	icon: "image/carpink (2).png",
 	
+  });
+ 
+    
+	var geocoder = new google.maps.Geocoder();
+	
+	document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
   });
 }
